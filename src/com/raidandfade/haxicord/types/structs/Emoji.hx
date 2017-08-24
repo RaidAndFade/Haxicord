b3bce294e3de154ae3bd10c17c30d81c3eb74def
RaidAndFade <business@gocode.it>
2017-08-24 17:06:23 -0400

    Emoji non-optional parameters.

--- src/com/raidandfade/haxicord/types/structs/Emoji.hx
+++ src/com/raidandfade/haxicord/types/structs/Emoji.hx
@@ -3,7 +3,7 @@ package com.raidandfade.haxicord.types.structs;
 typedef Emoji = {
     var id:String;
     var name:String;
-    var roles:Array<Role>;
-    var require_colons:Bool;
-    var managed:Bool;
+    @:optional var roles:Array<Role>;
+    @:optional var require_colons:Bool;
+    @:optional var managed:Bool;
 }
