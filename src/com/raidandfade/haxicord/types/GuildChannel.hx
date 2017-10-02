15c809c0f927140efc03a12273883d0aacab20ae
Thresher25 <12576332+Thresher25@users.noreply.github.com>
2017-10-01 21:38:44 -0400

    Added comments to GuildChannel.hx

--- src/com/raidandfade/haxicord/types/GuildChannel.hx
+++ src/com/raidandfade/haxicord/types/GuildChannel.hx
@@ -30,26 +30,51 @@ class GuildChannel extends Channel {
     }
 
     //Live structs
+
+    /**
+        Get the invites of a given channel
+        @param cb - Array of Invites (or error).
+     */
     public function getInvites(cb=null){
         client.endpoints.getChannelInvites(id.id,cb);
     }
-
+/**
+        Create a new invite for a given channel
+        @param invite_data - The invite data.
+        @param cb - Return the invite or an error.
+     */
     public function createInvite(invite_data,cb=null){
         client.endpoints.createChannelInvite(id.id,invite_data,cb);
     }
-
+/**
+        Change a channel's parameters
+        @param cd - The changed channel data, all fields are optional
+        @param cb - Callback to send the new channel object to. Or null if result is not desired.
+     */
     public function editChannel(cd,cb=null){
         client.endpoints.modifyChannel(id.id,cd,cb);
     }
-
+/**
+        Delete the given channel.
+        @param cb - Callback to send old channel to. Or null if result is not desired.
+     */
     public function deleteChannel(cb){
         client.endpoints.deleteChannel(id.id,cb);
     }
-
+/**
+        Edit or Create a channel's overwrite permissions;
+        @param perm - The modified overwrite permission object
+        @param pid - The overwrite Id, Id of user or role.
+        @param cb - Call once finished.
+     */
     public function editPermission(perm,pid,cb=null){
-        client.endpoints.editChannelPermissions(id.id,pid,cb);
+        client.endpoints.editChannelPermissions(id.id,pid,perm,cb);
     }
-
+/**
+        Delete a channel override
+        @param pid - The overwrite id to delete
+        @param cb - Call once finished.
+     */
     public function deletePermission(pid,cb=null){
         client.endpoints.deleteChannelPermission(id.id,pid,cb);
     }
