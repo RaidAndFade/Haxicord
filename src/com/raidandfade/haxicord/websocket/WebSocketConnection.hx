6857a00ca61866563fd76f6a8b6a3a3d9d87358f
RaidAndFade <business@gocode.it>
2018-01-02 03:09:50 -0500

    💄 Making it beautiful

--- src/com/raidandfade/haxicord/websocket/WebSocketConnection.hx
+++ src/com/raidandfade/haxicord/websocket/WebSocketConnection.hx
@@ -12,14 +12,14 @@ class WebSocketConnection {
 
     private static var ws:WebSocket;
     var queue:Array<String> = new Array<String>();
-    var ready=false;
+    var ready = false;
     private static var host:String;
     
     /**
         Connect to the given host (works for wss and ws)
         @param _host - The url of the host
      */
-    public function new(_host){ 
+    public function new(_host) { 
         host = _host;
 #if cpp
         var th = cpp.vm.Thread.create(create);
@@ -36,37 +36,47 @@ class WebSocketConnection {
     }
 
     @:dox(hide)
-    function create(){
+    function create() {
 #if cs
-        ws = new WebSocket(host,new cs.NativeArray<String>(0));
-        ws.add_OnOpen(new cs.system.EventHandler(function(f:Dynamic,e:cs.system.EventArgs){
-            ready=true;
-            for(m in queue){
-                send(m);
-            }
-            onReady();
-        }));
-        ws.add_OnMessage(new cs.system.EventHandler_1<websocketsharp.MessageEventArgs>(function(f:Dynamic,m:websocketsharp.MessageEventArgs){
-            this.onMessage(m.Data);
-        }));
-        ws.add_OnError(new cs.system.EventHandler_1<websocketsharp.MessageEventArgs>(function(f:Dynamic,m:websocketsharp.ErrorEventArgs){
-            this.onError(m.Message);
-        }));
-        ws.add_OnClose(new cs.system.EventHandler_1<websocketsharp.MessageEventArgs>(function(f:Dynamic,m:websocketsharp.ErrorEventArgs){
-            this._onClose();
-        }));
+        ws = new WebSocket(host, new cs.NativeArray<String>(0));
+        ws.add_OnOpen(
+            new cs.system.EventHandler( function(f:Dynamic, e:cs.system.EventArgs) {
+                ready = true;
+                for(m in queue) {
+                    send(m);
+                }
+                onReady();
+            })
+        );
+        ws.add_OnMessage(
+            new cs.system.EventHandler_1<websocketsharp.MessageEventArgs> ( function(f:Dynamic, m:websocketsharp.MessageEventArgs) {
+                this.onMessage(m.Data);
+            })
+        );
+        ws.add_OnError(
+            new cs.system.EventHandler_1<websocketsharp.MessageEventArgs> ( function(f:Dynamic, m:websocketsharp.ErrorEventArgs) {
+                this.onError(m.Message);
+            })
+        );
+        ws.add_OnClose( 
+            new cs.system.EventHandler_1<websocketsharp.MessageEventArgs> ( function(f:Dynamic, m:websocketsharp.ErrorEventArgs) {
+                this._onClose();
+            })
+        );
         ws.Connect();
 #else 
         ws = WebSocket.create(host, [], null, false);
-        ws.onopen = function(){
-            ready=true;
-            for(m in queue){
+        ws.onopen = function() {
+            ready = true;
+            for(m in queue) {
                 send(m);
             }
             onReady();
         }
-        ws.onmessageString = function(m){this.onMessage(m);}
-        ws.onmessageBytes = function(m){}
+        ws.onmessageString = function(m) { 
+            this.onMessage(m);
+        }
+        ws.onmessageBytes = function(m) {}
         ws.onerror = onError;
         ws.onclose = _onClose;
 #if sys
@@ -82,15 +92,15 @@ class WebSocketConnection {
         Send any object as json.
         @param d - The object to send.
      */
-    public function sendJson(d:Dynamic){
-        this.send(Json.stringify(d));
+    public function sendJson(d:Dynamic) {
+        this.send(Json.stringify(d) );
     }
     
     /**
         Send a raw string as a message.
         @param m - The string to send.
      */
-    public function send(m:String){
+    public function send(m:String) {
         //trace(m);
         if(!ready)
             queue.push(m);
@@ -103,7 +113,7 @@ class WebSocketConnection {
     }
 
 
-    private function _onClose(){
+    private function _onClose() {
         ready = false;
         onClose();
     }
@@ -111,23 +121,23 @@ class WebSocketConnection {
     /**
         Event listener for when the socket is closed
      */
-    dynamic public function onClose(){ }
+    dynamic public function onClose() { }
 
     /**
         Event listener for when the socket is open and connected
      */
-    dynamic public function onReady(){ }
+    dynamic public function onReady() { }
 
     /**
         Event listener for when the socket errors
         @param s - The error.
      */
-    dynamic public function onError(s){ }
+    dynamic public function onError(s) { }
 
     /**
         Event listener for when a message is received
         @param m - The message recieved;
      */
-    dynamic public function onMessage(m){ }
+    dynamic public function onMessage(m) { }
 
 }
