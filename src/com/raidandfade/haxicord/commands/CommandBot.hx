bf2c65d57a1eb728bf21d4f7ff240038bc60d49e
RaidAndFade <business@gocode.it>
2018-01-02 03:23:39 -0500

    💄 Making things more legible. Cleaning things up.

--- src/com/raidandfade/haxicord/commands/CommandBot.hx
+++ src/com/raidandfade/haxicord/commands/CommandBot.hx
@@ -8,63 +8,63 @@ import Type;
 
 class CommandBot {
     private var client:DiscordClient;
-    private var commands:Map<String,Command> = new Map();
+    private var commands:Map<String, Command> = new Map();
     private var prefix:String;
 
-    private function new(token:String,commandBot:Class<CommandBot>,_prefix="!"){
+    private function new(token:String, commandBot:Class<CommandBot>, _prefix = "!") {
         prefix = _prefix;
         client = new DiscordClient(token);
         client.onMessage = onMessage;
         client.onReady = function(){
-            trace("My invite link is: "+client.getInviteLink());
+            trace("My invite link is: " + client.getInviteLink());
         }
 
         var annr = Meta.getFields(commandBot);
-        for(comName in Reflect.fields(annr)){
-            var com = Reflect.field(annr,comName);
+        for(comName in Reflect.fields(annr)) {
+            var com = Reflect.field(annr, comName);
             var params:CommandParams = null;
-            for(annName in Reflect.fields(com)){
-                if(annName == "Command"){
-                    try{
-                        var p = Reflect.field(com,annName);
-                        if(p==null)
+            for(annName in Reflect.fields(com)) {
+                if(annName == "Command") {
+                    try {
+                        var p = Reflect.field(com, annName);
+                        if(p == null)
                             params = new CommandParams();
                         else
                             params = p[0]; //i wish i knew why [0].
-                    }catch(e:Dynamic){
-                        throw "Command "+com+"'s annotation is not correctly formatted.";
+                    } catch(e:Dynamic) {
+                        throw "Command " + com + "'s annotation is not correctly formatted.";
                     }
                 }
             }
-            if(params!=null){
-                var func = Reflect.field(this,comName);
-                trace("Registered "+comName+" Command");
-                registerCommand(comName,params,func);
+            if(params != null) {
+                var func = Reflect.field(this, comName);
+                trace("Registered " + comName + " Command");
+                registerCommand(comName, params, func);
             }
         }
         client.start();
     }
 
-    private function onMessage(m:Message){
+    private function onMessage(m:Message) {
         var cnt = m.content;
-        if(cnt.substr(0,prefix.length)==prefix){
+        if(cnt.substr(0, prefix.length) == prefix) {
             cnt = cnt.substr(prefix.length);
             var cmd = cnt.split(" ")[0];
-            if(commands.exists(cmd)){
-                callCommand(cmd,m);
+            if(commands.exists(cmd)) {
+                callCommand(cmd, m);
             }
         }
     }
 
-    private function registerCommand(commandName:String,commandParams:CommandParams,commandFunc:Dynamic->Void){
-        commands.set(commandName,{func:commandFunc,params:commandParams});
+    private function registerCommand(commandName:String, commandParams:CommandParams, commandFunc:Dynamic->Void) {
+        commands.set(commandName, {func:commandFunc, params:commandParams});
     }
 
-    private function callCommand(commandName:String,message:Message){
+    private function callCommand(commandName:String, message:Message) {
         var args:Array<Dynamic> = [];
         var com = commands.get(commandName);
-        if(com.params.context==true)
-            args.push({client:client,channel:message.getChannel()});
+        if(com.params.context == true)
+            args.push({client:client, channel:message.getChannel()});
         args.push(message);
         Reflect.callMethod(this, com.func, args);
     }
@@ -76,10 +76,10 @@ typedef Command = {
 }
 
 class CommandParams {
-    public var context:Bool=false;
-    public var permissions:Int=0;
+    public var context:Bool = false;
+    public var permissions:Int = 0;
 
-    public function new(){}
+    public function new() {}
 }
 
 typedef CommandContext = {
